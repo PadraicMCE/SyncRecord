d3d1ec8899cf002bdd85a0d54f86d17875dff0e0
Padraic Mcevoy <455206@dit.ie>
2023-11-01 15:24:09 +0000

    Added file to repo: public/js/Record.js

---
+++ public/js/Record.js
@@ -0,0 +1,52 @@
+class recordProcessor extends AudioWorkletProcessor
+{
+  static get parameterDescriptors() {
+    return [{
+      name: 'isRecording',
+      defaultValue: 0
+    }];
+  }
+  constructor()
+    {
+      super();
+      this.port.onmessage = (e) => 
+			{
+        //Count numer of samples sent
+        this.sampleCounter = 0;
+        // Continuously send data back to the min thread.
+				if (e.data.eventType === 'Start') 
+				{
+          console.log('WorkletAudio received start command');
+					this.iterations == 0;
+          this.shouldRecord = 1;
+          this.port.postMessage({
+            eventType: 'started',
+          });
+				}
+        if (e.data.eventType === 'Stop') 
+				{
+          console.log('WorkletAudio received stop command');
+					this.shouldRecord = 0;
+          this.port.postMessage({
+            eventType: 'stopped',
+            timedate: e.data.timedate
+          });
+				}
+      };
+    }
+
+    process(inputs,ouputs,parameters)
+    {
+      const input = inputs[0];
+      const inputChannel = input[0];
+        this.port.postMessage({
+          eventType: 'data',
+          totalSamples: this.sampleCounter,
+          audioBuffer: inputChannel
+        });
+      this.sampleCounter += inputChannel.length;
+      return true;
+    }
+
+}
+registerProcessor('recorder-worklet', recordProcessor);

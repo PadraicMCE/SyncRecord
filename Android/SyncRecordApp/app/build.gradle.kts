66114467d5c19f2f3933360cf641cdd09147ebc1
PadraicMCE <padraicmcevoy88@gmail.com>
2026-08-06 17:02:37 +0100

    updated gradle

--- Android/SyncRecordApp/app/build.gradle.kts
+++ Android/SyncRecordApp/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
